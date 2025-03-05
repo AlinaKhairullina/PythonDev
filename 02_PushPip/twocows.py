3e24c11f7a08840feb87005c0fdfa2a36c4f98f8
Alina Khairullina <alinakhairullina200407@gmail.com>
2025-03-05 16:30:32 +0300

    Add -n and -N flags

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -5,19 +5,21 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-f', default='default')
     parser.add_argument('-e', default='oo')
-    parser.add_argument('-n', action='store_true')
+    parser.add_argument('-n', action='store_false')
     parser.add_argument('-F', default='default')
     parser.add_argument('-E', default='oo')
-    parser.add_argument('-N', action='store_true')
+    parser.add_argument('-N', action='store_false')
     parser.add_argument('msg_1', type=str)
     parser.add_argument('msg_2', type=str)
     args = parser.parse_args()
     cow1 = cowsay(message=args.msg_1,
                   cow=args.f,
-                  eyes=args.e).split("\n")
+                  eyes=args.e, 
+                  wrap_text=args.n).split("\n")
     cow2 = cowsay(message=args.msg_2,
                   cow=args.F,
-                  eyes=args.E).split("\n")
+                  eyes=args.E,
+                  wrap_text=args.N).split("\n")
     strlen1, strlen2 = len(cow1), len(cow2)
     if strlen1 > strlen2:
         print('\n'.join(i for i in cow1[:strlen1 - strlen2]))
